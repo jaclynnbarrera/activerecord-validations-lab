8a693b83e0dfc04513a4314826984d494e768840
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-03-19 13:26:59 -0400

    commit tree bbda208d72ad89a34fc9a3ae8f8a4a67b70022b3

--- app/validators/non_clickbait_validator.rb
+++ app/validators/non_clickbait_validator.rb
@@ -1,17 +1,10 @@
-class NonClickbaitValidator < ActiveModel::Validator
-    def validate(record)
-        words = ["Won't Believe","Secret","Top [number]", "Guess"]
-        unless words.include?(record.title)
-            record.errors.add :name, false
-        end
-    end
-end
-
-# def validate(record)
-    #     words = ["Won't Believe","Secret","Top [number]", "Guess"]
-    #     if words.any?(record.title)
-    #         errors.add(:title, false)
-    #     else
-    #         true
-    #     end
-    # end   
+# class NonClickbaitValidator < ActiveModel::Validator
+#     def validate(record)
+#         words = ["Won't Believe","Secret","Top [number]", "Guess"]
+#         if words.include?(record.title)
+#             record.errors.add :title, false
+#         else
+#             false
+#         end
+#     end
+# end
