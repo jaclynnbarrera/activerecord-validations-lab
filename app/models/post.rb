22e5dbfc04f22d9c743da26cbba7690b19c8adf7
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-03-18 23:11:39 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+    include ActiveModel::Validations
+    validates_with NonClickbaitValidator
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, presence: true, inclusion: { in: ["Fiction", "Non-Fiction"]}
 end
